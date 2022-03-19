5383a7a1b7c49f7e35f326e8d2296696bdd9f006
attikoo <attikoo@coventry.ac.uk>
2022-03-19 17:09:22 +0000

    Get method

--- server.dart
+++ server.dart
@@ -2,49 +2,55 @@
 
 /* the simplest possible web server */
 
-import 'dart:io' show InternetAddress;
-
-import 'package:shelf/shelf.dart' show Request, Response;
-import 'package:shelf/shelf_io.dart' show serve;
-/* import 'package:shelf_router/shelf_router.dart' show Router; */
-// import 'package:mysql1/mysql1.dart';
+import 'dart:io';
+import 'package:mysql1/mysql1.dart';
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart';
+import 'package:shelf_router/shelf_router.dart';
 import 'package:mysql_client/mysql_client.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
 Future main() async {
-  // final connection = await MySqlConnection.connect(new ConnectionSettings(
-  //   host: 'localhost',
-  //   port: 3307,
-  //   user: 'root',
-  //   password: 'fatoki@dimeji1A',
-  //   db: 'faqs',
-  // ));
   final connection = await MySQLConnection.createConnection(
     host: "localhost",
     port: 3307,
     userName: "root",
     password: "fatoki@dimeji1A",
-    databaseName: "faqs", // optional
+    databaseName: "faqs",
   );
   await connection.connect();
-  await connection.execute(
-      'insert into questions(title, summary, description) values("name","jndckdck","kdjcbkd")');
-  // await connection.execute('delete from questions where title = "name";');
-  // var results = await connection.execute('select * from questions');
-  var result = await connection.execute('select * from answers');
-  // print(result);
-  print(result.rows);
-  for (var row in result.rows) {
-    print('${row.colAt(1)}');
-  }
 
-  await connection.close();
-  /*final server = await serve(
-    helloWorld,
+  Router router = Router();
+
+  router.get('/faqs', (Request request) async {
+    var results = await connection.execute("select * from questions");
+
+    for (final row in results.rows) {
+      print(row.assoc());
+    }
+    var myList = results.rows.toList();
+    print(myList);
+    return await Response.ok(jsonEncode(myList));
+
+    //Map<String, dynamic> jsonResponse = convert.jsonDecode(response.body);
+    // List<String> results = new List.empty()
+
+    // String encodedData = convert.jsonEncode(jsonResponse['questions'][0]);
+    // print(encodedData);
+    // return await Response.ok(jsonEncode(load));
+  });
+
+  // await connection.close();
+
+  final server = await serve(
+    router,
     InternetAddress.anyIPv4,
     8080,
-  );*/
+  );
 
-  /* print('Serving at http://${server.address.host}:${server.port}');*/
+  print('Serving at http://${server.address.host}:${server.port}');
 }
 
-/*Response helloWorld(Request request) => Response.ok('Hello World!');*/
+// Response request(Request request) => Response.ok('result');
